4ff5444dcee89c5fd56ccd43155c5249b0a8c1da
AllusionGleb <lapingleb214@gmail.com>
2024-10-31 11:21:03 +0300

    dfg

---
+++ app/src/main/java/com/example/mealjsonexample/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.mealjsonexample.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Новая цветовая палитра
+val Blue80 = Color(0xFF1E88E5) // Основной цвет
+val Orange80 = Color(0xFFFF9800) // Вторичный цвет
+val Green80 = Color(0xFF4CAF50) // Третичный цвет
+
+val Blue40 = Color(0xFF1E88E5) // Основной цвет для темной темы
+val Orange40 = Color(0xFFFF9800) // Вторичный цвет для темной темы
+val Green40 = Color(0xFF4CAF50) // Третичный цвет для темной темы
